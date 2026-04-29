af85140b99ba8a34f3c4389b0bc9129e62e2e486
Rifsya.Z <rifsya@gmail.com>
2026-04-29 18:57:22 +0700

    aa

--- MASTER/MASTER_V3/MASTER_V3.ino
+++ MASTER/MASTER_V3/MASTER_V3.ino
@@ -40,53 +40,7 @@ void loop() {
   fedback();
   Debug_odometry();
   CommunicationESP();
-  if (W && D) {
-    Serial.println("MAJU KANAN");
-    SwerveDrive(0.3, -0.3, 0, 30);
-  } else if (W && A) {
-    Serial.println("MAJU Kiri");
-    SwerveDrive(0.3, 0.3, 0, 30);
-  } else if (S && D) {
-    Serial.println("Mundur KANAN");
-    SwerveDrive(-0.3, -0.3, 0, 30);
-  } else if (S && A) {
-    Serial.println("Mundur Kiri");
-    SwerveDrive(-0.3, 0.3, 0, 30);
-  } else if (W) {
-    Serial.println("MAJU");
-    SwerveDrive(0.3, 0, 0, 30);
-  } else if (S) {
-    Serial.println("Mundur");
-    SwerveDrive(-0.3, 0, 0, 30);
-  } else if (A) {
-    Serial.println("kiri");
-    SwerveDrive(0, 0.3, 0, 30);
-  } else if (D) {
-    Serial.println("kanan");
-    SwerveDrive(0, -0.3, 0, 30);
-  } else if (J) {
-    SwerveDrive(0, 0, 0.3, 30);
-    Serial.println("CCW");
-  } else if (K) {
-    SwerveDrive(0, 0, -0.3, 30);
-    Serial.println("CW");
-  } else if (STOP) {
-    STOP_ALL();
-    Serial.println("Stop");
-  } else if (P) {
-    STOP_ALL();
-    Serial.println("Stop");
-  } else if (H) {
-    HOME_ALL();
-    Serial.println("HOMING");
-  } else if (P1) {
-    ModeBuzzer(TULULIT);
-  } else if (P2) {
-    ModeBuzzer(Armed);
-  }
-}
-
-// if (Serial.available()) {
+  // if (Serial.available()) {
 //   String cmd = Serial.readStringUntil('\n');
 //   cmd.trim();
 //   if (cmd.length() == 0) return;
@@ -100,3 +54,4 @@ void loop() {
 //     HOME_ALL();
 //   }
 // }
+}
